84fdf46d80c42be16e6628808a550c2d5349653b
GKoss <gorbunov-k.o@ya.ru>
2018-06-06 22:22:46 +0300

    task1

---
+++ script.js
@@ -0,0 +1,16 @@
+var money = prompt('Ваш бюджет?', 500);
+var name = prompt('Название Вашего магазина?', '');
+var mainList = {
+    budget: money,
+    shopName: name,
+    shopGoods: [],
+    employers: {},
+    open: false
+};
+
+mainList.shopGoods[0] = prompt('Какой тип товаров будем продавать?', '');
+mainList.shopGoods[1] = prompt('Какой тип товаров будем продавать?', '');
+mainList.shopGoods[2] = prompt('Какой тип товаров будем продавать?', '');
+
+alert(mainList.budget / 30);
+console.log(mainList);
